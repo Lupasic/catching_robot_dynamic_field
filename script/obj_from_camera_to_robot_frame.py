0581eb3cd43d0c51303593d41acd4a418f6d99e6
Lupasic <obulichev@yandex.ru>
2019-06-19 17:37:12 +0300

    All needed camera nodes are ready (from pixel to 3d point in camera frame, tf to robot frame, transform 3d point from camera to robot base frame

--- script/obj_from_camera_to_robot_frame.py
+++ script/obj_from_camera_to_robot_frame.py
@@ -3,17 +3,20 @@
 import rospy
 import tf2_geometry_msgs  # import the packages first
 import tf2_ros
-from geometry_msgs.msg import PointStamped, Point
+from geometry_msgs.msg import PointStamped
 
 pub = rospy.Publisher('obj_from_camera_to_robot_frame', PointStamped, queue_size=10)
 
 
 def TFcallback(data):
     global pub, tf_buffer
-    transform = tf_buffer.lookup_transform("robot_base",
-                                           data.header.frame_id,  # source frame
-                                           rospy.Time(0),  # get the tf at first available time
-                                           rospy.Duration(1.0))
+    try:
+        transform = tf_buffer.lookup_transform("robot_base",
+                                               data.header.frame_id,  # source frame
+                                               rospy.Time(0),  # get the tf at first available time
+                                               rospy.Duration(1.0))
+    except Exception:
+        return 0
     point_stamped_transformed = PointStamped()
     point_stamped_transformed.header.stamp = rospy.Time.now()
     point_stamped_transformed.header.frame_id = "robot_base"
