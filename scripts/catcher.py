1502d24d2d93dfd7e360a9f29a2acc196d8b9afb
Oleg Balakhnov <hop9999@mail.ru>
2019-06-19 19:41:00 +0300

    integration between scripts and ros tests

--- scripts/catcher.py
+++ scripts/catcher.py
@@ -7,10 +7,11 @@ from kalman_data import *
 import rospy
 from geometry_msgs.msg import Point
 
+
 class Catcher:
     def __init__(self):
         rospy.Subscriber( "red_ball_xyz", Point, self.callback )
-
+        self.tracker = Kalman()
         self.ac = 4
         self.v = 2
         self.x_catch = np.array( [ [ 0. ],
@@ -23,29 +24,53 @@ class Catcher:
         self.trans = self.robot.get_pose()
         self.robot_init()
         self.prev_time = time.time()
+        print( "ready" )
 
     def __del__(self):
         self.robot.close()
 
     def callback(self, data):
+        clb_s = time.time()
         x = data.x
         y = data.y
         z = data.z
-
+        print( data )
         act_time = time.time()
         dt = act_time - self.prev_time
+        #print(dt)
         self.prev_time = act_time
-
-        trajectory = tracker.trajectory_identification( x, y, z, dt )
+        img = np.zeros( (500, 1000, 3), np.uint8 )
+        trajectory = self.tracker.trajectory_identification( x, y, z, dt )
         if trajectory.any():
             pr_x = self.predict_catch_point( trajectory, 0.2 )
-            d = np.linalg.norm( pr_x - self.x_catch )
-            if d > 0.04 and self.coord_valid( pr_x ) == True:
-                self.x_catch = pr_x
-                self.trans.pos.x = pr_x[ 0 ][ 0 ]
-                self.trans.pos.y = pr_x[ 1 ][ 0 ]
-                self.trans.pos.z = pr_x[ 2 ][ 0 ]
-                self.robot.set_pose( self.trans, acc=self.ac, vel=self.v, wait=False, threshold=0.1 )
+            if pr_x.any():
+                d = np.linalg.norm( pr_x - self.x_catch )
+                if d > 0.04 and self.coord_valid( pr_x ) == True:
+                    self.x_catch = pr_x
+                    self.trans.pos.x = pr_x[ 0 ][ 0 ]
+                    self.trans.pos.y = pr_x[ 1 ][ 0 ]
+                    self.trans.pos.z = pr_x[ 2 ][ 0 ]
+                    self.robot.set_pose( self.trans, acc=self.ac, vel=self.v, wait=False, threshold=0.1 )
+            xx = trajectory[ 0 ]
+            dxx = trajectory[ 1 ]
+            yy = trajectory[ 2 ]
+            dyy = trajectory[ 3 ]
+            zz = trajectory[ 4 ]
+            dzz = trajectory[ 5 ]
+
+            cv2.circle( img, (int( x * 100 ), int( -100 * z ) + 600), 5, (0, 0, 255), 2 )
+            cv2.circle( img, (int( y * 100 ) + 500, int( -100 * z ) + 500), 5, (0, 0, 255), 2 )
+
+            for i in range( 300 ):
+                xxx = int( (xx + dxx * i * 0.005) * 100 )
+                yyy = int( (yy + dyy * i * 0.005) * 100 + 500 )
+                zzz = int( -(zz + dzz * i * 0.005 - 9.8 / 2 * (0.005 * i) ** 2) * 100 + 500 )
+                cv2.circle( img, (xxx, zzz), 5, (255, 0, 0), 2 )
+                cv2.circle( img, (yyy, zzz), 5, (255, 0, 0), 2 )
+
+            cv2.imshow( 'Frame', img )
+            cv2.waitKey( 1 )
+        print(time.time() - clb_s)
 
     def robot_init(self):
         self.trans.pos.x = 0.0
@@ -58,9 +83,9 @@ class Catcher:
         self.robot.set_pose( self.trans, acc=self.ac, vel=self.v, wait=True )
         time.sleep( 1 )
 
-    def coord_valid(self,x):
-        if x[ 0 ][ 0 ] < -0.5 or x[ 0 ][ 0 ] > 0.5: return False
-        if x[ 1 ][ 0 ] < 0 or x[ 1 ][ 0 ] > 1: return False
+    def coord_valid(self, x):
+        if x[ 0 ][ 0 ] < -0.4 or x[ 0 ][ 0 ] > 0.4: return False
+        if x[ 1 ][ 0 ] < 0.2 or x[ 1 ][ 0 ] > 1: return False
         return True
 
     def predict_catch_point(self, X, z_d):
@@ -71,10 +96,9 @@ class Catcher:
         t_f = -(-X[ 5 ][ 0 ] - np.sqrt( X[ 5 ][ 0 ] ** 2 + 2 * 9.8 * (X[ 4 ][ 0 ] - z_d) )) / 9.8
         x_p[ 0 ][ 0 ] = X[ 0 ][ 0 ] + X[ 1 ][ 0 ] * t_f
         x_p[ 1 ][ 0 ] = X[ 2 ][ 0 ] + X[ 3 ][ 0 ] * t_f
+        if t_f < 0: return np.array( [ [ ] ] )
         return x_p
 
-
-tracker = Kalman()
 
 rospy.init_node( 'listener', anonymous=True )
 catcher = Catcher()
---
+++ scripts/sim_catch.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import os
+import time
+import rospy
+from geometry_msgs.msg import Point
+
+pub = rospy.Publisher( 'red_ball_xyz', Point, queue_size=10 )
+rospy.init_node( 'talker', anonymous=True )
+rate = rospy.Rate( 30 )  # 10hz
+g = -9.8
+start_time = time.time()
+prev_time = start_time
+
+X = np.array( [ [ 0.1 ],
+                [ 0.0 ],
+                [ 6.4 ],
+                [ -3. ],
+                [ 0.1 ],
+                [ 9.5 ] ] )
+
+while not rospy.is_shutdown():
+    act_time = time.time()
+    dt = act_time - prev_time
+    print(dt)
+    prev_time = act_time
+    X[ 0 ][ 0 ] += X[ 1 ][ 0 ] * dt
+    X[ 2 ][ 0 ] += X[ 3 ][ 0 ] * dt
+    X[ 4 ][ 0 ] += X[ 5 ][ 0 ] * dt + g * dt ** 2 / 2
+    X[ 5 ][ 0 ] += g * dt
+    if act_time - start_time > 2.5:break
+    current_point = Point( X[ 0 ][ 0 ], X[ 2 ][ 0 ], X[ 4 ][ 0 ] )
+    pub.publish( current_point )
+
+    rate.sleep()
