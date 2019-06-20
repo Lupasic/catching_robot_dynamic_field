622026e9eb4632c700776569b0b8ca3c796fc6b9
Lupasic <obulichev@yandex.ru>
2019-06-20 10:55:40 +0300

    merge was succeded

--- scripts/kalman_data.py
+++ scripts/kalman_data.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import cv2
 import numpy as np
 
@@ -13,46 +15,46 @@ class Kalman:
         self.prev_y = 0
         self.prev_z = 0
 
-        self.kalman = cv2.KalmanFilter( 6, 3, 1 )
+        self.kalman = cv2.KalmanFilter(6, 3, 1)
 
-        self.kalman.transitionMatrix = np.array( [ [ 1., self.dt, 0., 0., 0., 0. ],
-                                                   [ 0., 1., 0., 0., 0., 0. ],
-                                                   [ 0., 0., 1., self.dt, 0., 0. ],
-                                                   [ 0., 0., 0., 1., 0., 0. ],
-                                                   [ 0., 0., 0., 0., 1., self.dt ],
-                                                   [ 0., 0., 0., 0., 0., 1. ] ] )
+        self.kalman.transitionMatrix = np.array([[1., self.dt, 0., 0., 0., 0.],
+                                                 [0., 1., 0., 0., 0., 0.],
+                                                 [0., 0., 1., self.dt, 0., 0.],
+                                                 [0., 0., 0., 1., 0., 0.],
+                                                 [0., 0., 0., 0., 1., self.dt],
+                                                 [0., 0., 0., 0., 0., 1.]])
 
-        self.kalman.measurementMatrix = np.array( [ [ 1., 0., 0., 0., 0., 0. ],
-                                                    [ 0., 0., 1., 0., 0., 0. ],
-                                                    [ 0., 0., 0., 0., 1., 0. ] ] )
+        self.kalman.measurementMatrix = np.array([[1., 0., 0., 0., 0., 0.],
+                                                  [0., 0., 1., 0., 0., 0.],
+                                                  [0., 0., 0., 0., 1., 0.]])
 
-        self.kalman.controlMatrix = np.array( [ [ 0. ],
-                                                [ 0. ],
-                                                [ 0. ],
-                                                [ 0. ],
-                                                [ 0. ],
-                                                [ self.dt ** 2 / 2 ] ] )
+        self.kalman.controlMatrix = np.array([[0.],
+                                              [0.],
+                                              [0.],
+                                              [0.],
+                                              [0.],
+                                              [self.dt ** 2 / 2]])
 
-        self.kalman.processNoiseCov = np.array( [ [ 1e-1, 0., 0., 0., 0., 0. ],
-                                                  [ 0., 1e-1, 0., 0., 0., 0. ],
-                                                  [ 0., 0., 1e-1, 0., 0., 0. ],
-                                                  [ 0., 0., 0., 1e-1, 0., 0. ],
-                                                  [ 0., 0., 0., 0., 1e-1, 0. ],
-                                                  [ 0., 0., 0., 0., 0., 1e-1 ] ] )
+        self.kalman.processNoiseCov = np.array([[1e-1, 0., 0., 0., 0., 0.],
+                                                [0., 1e-1, 0., 0., 0., 0.],
+                                                [0., 0., 1e-1, 0., 0., 0.],
+                                                [0., 0., 0., 1e-1, 0., 0.],
+                                                [0., 0., 0., 0., 1e-1, 0.],
+                                                [0., 0., 0., 0., 0., 1e-1]])
 
-        self.kalman.measurementNoiseCov = 1e+2 * np.eye( 3 )
+        self.kalman.measurementNoiseCov = 1e+2 * np.eye(3)
 
-        self.kalman.errorCovPost = 1e6 * np.eye( 6 )
+        self.kalman.errorCovPost = 1e6 * np.eye(6)
 
     def update_kalman_matrices(self, dt):
         self.dt = dt
-        self.kalman.transitionMatrix[ 0 ][ 1 ] = self.dt
-        self.kalman.transitionMatrix[ 2 ][ 3 ] = self.dt
-        self.kalman.transitionMatrix[ 4 ][ 5 ] = self.dt
-        self.kalman.controlMatrix[ 5 ] = self.dt
+        self.kalman.transitionMatrix[0][1] = self.dt
+        self.kalman.transitionMatrix[2][3] = self.dt
+        self.kalman.transitionMatrix[4][5] = self.dt
+        self.kalman.controlMatrix[5] = self.dt
 
     def trajectory_identification(self, x, y, z, dt):
-        self.update_kalman_matrices( dt )
+        self.update_kalman_matrices(dt)
         g = -9.8
 
         if self.flag_init_data == 0:
@@ -60,29 +62,29 @@ class Kalman:
             self.prev_y = y
             self.prev_z = z
             self.flag_init_data = 1
-            return np.array( [ [ ] ] )
+            return np.array([[]])
 
         if self.flag_start_tracking == 0:
             vx = (x - self.prev_x) / dt
             vy = (y - self.prev_y) / dt
             vz = (z - self.prev_z) / dt
-            print( vx, vy, vz )
+            print(vx, vy, vz)
 
-            self.kalman.statePost = np.array( [ [ x ],
-                                                [ vx ],
-                                                [ y ],
-                                                [ vy ],
-                                                [ z ],
-                                                [ vz ] ] )
+            self.kalman.statePost = np.array([[x],
+                                              [vx],
+                                              [y],
+                                              [vy],
+                                              [z],
+                                              [vz]])
             self.flag_start_tracking = 1
 
-        meas = np.array( [ [ x ],
-                           [ y ],
-                           [ z ] ] )
+        meas = np.array([[x],
+                         [y],
+                         [z]])
 
-        control = np.array( [ [ g ] ] )
+        control = np.array([[g]])
 
-        prediction = self.kalman.predict( control )
-        correct = self.kalman.correct( meas )
+        prediction = self.kalman.predict(control)
+        correct = self.kalman.correct(meas)
 
         return correct
--- scripts/sim_catch.py
+++ scripts/sim_catch.py
@@ -1,35 +1,36 @@
-import cv2
-import numpy as np
-import os
+#!/usr/bin/env python
+
 import time
+
+import numpy as np
 import rospy
 from geometry_msgs.msg import Point
 
-pub = rospy.Publisher( 'red_ball_xyz', Point, queue_size=10 )
-rospy.init_node( 'talker', anonymous=True )
-rate = rospy.Rate( 30 )  # 10hz
+pub = rospy.Publisher('red_ball_xyz', Point, queue_size=10)
+rospy.init_node('talker', anonymous=True)
+rate = rospy.Rate(30)  # 10hz
 g = -9.8
 start_time = time.time()
 prev_time = start_time
 
-X = np.array( [ [ 0.1 ],
-                [ 0.0 ],
-                [ 6.4 ],
-                [ -3. ],
-                [ 0.1 ],
-                [ 9.5 ] ] )
+X = np.array([[0.1],
+              [0.0],
+              [6.4],
+              [-3.],
+              [0.1],
+              [9.5]])
 
 while not rospy.is_shutdown():
     act_time = time.time()
     dt = act_time - prev_time
     print(dt)
     prev_time = act_time
-    X[ 0 ][ 0 ] += X[ 1 ][ 0 ] * dt
-    X[ 2 ][ 0 ] += X[ 3 ][ 0 ] * dt
-    X[ 4 ][ 0 ] += X[ 5 ][ 0 ] * dt + g * dt ** 2 / 2
-    X[ 5 ][ 0 ] += g * dt
-    if act_time - start_time > 2.5:break
-    current_point = Point( X[ 0 ][ 0 ], X[ 2 ][ 0 ], X[ 4 ][ 0 ] )
-    pub.publish( current_point )
+    X[0][0] += X[1][0] * dt
+    X[2][0] += X[3][0] * dt
+    X[4][0] += X[5][0] * dt + g * dt ** 2 / 2
+    X[5][0] += g * dt
+    if act_time - start_time > 2.5: break
+    current_point = Point(X[0][0], X[2][0], X[4][0])
+    pub.publish(current_point)
 
     rate.sleep()
